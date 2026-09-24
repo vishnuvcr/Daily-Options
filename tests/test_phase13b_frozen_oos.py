a8c8d94a466eebd91d0883ebe91719ba2b083fe6
vishnuvcr <72708604+vishnuvcr@users.noreply.github.com>
2026-09-24 17:24:43 +0530

    phase13b: add frozen-rule tests

---
+++ tests/test_phase13b_frozen_oos.py
@@ -0,0 +1,7 @@
+from research.phase13b_frozen_oos import SIGNAL_TIME, Z_THRESHOLD, RV_PERCENTILE, HOLD_MINUTES
+
+def test_frozen_rule_constants():
+    assert SIGNAL_TIME == '14:45:00'
+    assert Z_THRESHOLD == 1.5
+    assert RV_PERCENTILE == 80.0
+    assert HOLD_MINUTES == 10
